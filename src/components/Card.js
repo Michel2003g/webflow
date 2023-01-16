a50286409bf72b5a6b77798b2f05cd0dbb9bba31
Michel2003g <111076051+Michel2003g@users.noreply.github.com>
2023-01-16 13:04:11 +0100

    open links

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,14 @@
 const Card = ({data}) => {
+
+  function openSequance() {
+    data.links.map( link => {
+      console.log(link)
+      window.open(link, "_blank");
+    })
+  }
+
   return (
-    <div className="card">
+    <div className="card" onClick={openSequance}>
         <p className="title">{data.name}</p>
     </div>
   )
